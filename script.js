a8ae5aaf157bcc0ae3ca8b6198e9b9f26d6fe678
wvalverder <wvalverder@gmail.com>
2023-11-06 15:08:53 -0500

    Agregar el enlace script

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const h2 = document.querySelector('h2');
 const input1 = document.querySelector('#calculo1');
 const btn = document.querySelector('#btnCalcular');
 
+
 btn.addEventListener('click', btnOnClick);
 
 function btnOnClick() {
