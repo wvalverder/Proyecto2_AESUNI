fb7935f0636be995f9c089892f52dd75c5b74046
wvalverder <wvalverder@gmail.com>
2023-11-06 16:02:48 -0500

    Finalizacion del programa en JS

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const h2 = document.querySelector('h2');
 const input1 = document.querySelector('#calculo1');
 const btn = document.querySelector('#btnCalcular');
+const parrafo1 = document.createElement('p');
+const parrafo2 = document.createElement('p');
+const parrafo3 = document.createElement('p');
+const parrafo4 = document.createElement('p'); 
+const parrafo5 = document.createElement('p');
+const parrafo6 = document.createElement('p');
+const parrafo7 = document.createElement('p'); 
+const mostrarParent = document.querySelector('.MostrarCalculos'); 
+
 
 
 btn.addEventListener('click', btnOnClick);
@@ -15,4 +24,12 @@ function btnOnClick() {
     const PesoUrano = PesoPlanetaTierra * 1.05;
     const PesoNeptuno = PesoPlanetaTierra * 1.23;
     h2.innerHTML = 'El Peso en otros planetas es :';
+    mostrarParent.append(parrafo1,parrafo2,parrafo3,parrafo4,parrafo5,parrafo6,parrafo7);
+    parrafo1.innerHTML = 'Venus : ' + PesoVenus + 'kg';
+    parrafo2.innerHTML = 'Mercurio : ' + PesoMercurio + 'kg';
+    parrafo3.innerHTML = 'Marte : ' + PesoMarte + 'kg';
+    parrafo4.innerHTML = 'Jupiter : ' + PesoJupiter + 'kg';
+    parrafo5.innerHTML = 'Saturno : ' + PesoSaturno + 'kg';
+    parrafo6.innerHTML = 'Urano : ' + PesoUrano + 'kg';
+    parrafo7.innerHTML = 'Neptuno : ' + PesoNeptuno + 'kg';
 }
