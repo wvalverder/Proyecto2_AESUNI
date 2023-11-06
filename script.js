894058921f1bfad2ee974aa2a126d8a70968be53
wvalverder <wvalverder@gmail.com>
2023-11-06 14:32:08 -0500

    agregar comandos en js

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const h2 = document.querySelector('h2');
 const input1 = document.querySelector('#calculo1');
 const btn = document.querySelector('#btnCalcular');
 
+btn.addEventListener('click', btnOnClick);
+
 function btnOnClick() {
-    
+    const PesoPlanetaTierra = parseFloat(input1.value);
+    const PesoVenus = PesoPlanetaTierra * 0.78;
+    const PesoMercurio = PesoPlanetaTierra * 0.38;
+    const PesoMarte = PesoPlanetaTierra * 0.39; 
+    const PesoJupiter = PesoPlanetaTierra * 2.65;
+    const PesoSaturno = PesoPlanetaTierra * 1.17;
+    const PesoUrano = PesoPlanetaTierra * 1.05;
+    const PesoNeptuno = PesoPlanetaTierra * 1.23;
+    h2.innerHTML = 'El Peso en otros planetas es :';
 }
