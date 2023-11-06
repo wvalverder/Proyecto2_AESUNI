8636dc593c5289e82cd3c961ce5abc374b9cce3e
wvalverder <wvalverder@gmail.com>
2023-11-06 14:11:01 -0500

    creando la plantilla HTML , algunos estilos y JS

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = document.querySelector('h2');
+const input1 = document.querySelector('#calculo1');
+const btn = document.querySelector('#btnCalcular');
+
+function btnOnClick() {
+    
+}
